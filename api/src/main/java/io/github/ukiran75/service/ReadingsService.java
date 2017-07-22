a10832004eab0328a3aae653791e8b7dc930a505
Uday Talapaneni <ukiran75@gmail.com>
2017-07-22 03:05:48 -0500

    Revert "feat(module-api): Submit api module"

--- api/src/main/java/io/github/ukiran75/service/ReadingsService.java
+++
@@ -1,9 +0,0 @@
-package io.github.ukiran75.service;
-
-/*
- * Reading Service Interface
- */
-public interface ReadingsService {
-    void insertReadings(String readings);
-    String getReadingsofVehicle(String vin);
-}
